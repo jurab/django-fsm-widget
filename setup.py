0e2424b45aac6badddec573f76ff6494bd42364a
Ashley Mathew <ashley@directemployersfoundation.org>
2014-08-06 11:33:19 -0400

    Switched to MIT-only license.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
 
     classifiers = [
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
     ],
